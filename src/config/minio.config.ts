a66bd77ed1bdc50e86ebfbf934e970f99bd4af2f
ducvubo <vminhduc8@gmail.com>
2025-02-10 09:55:28 +0700

    update minio

--- src/config/minio.config.ts
+++ src/config/minio.config.ts
@@ -15,8 +15,8 @@ export const initMinio = () => {
     endPoint: '160.191.51.57',
     port: 9000,
     useSSL: false,
-    accessKey: '0u3Cu2pEQwjlx9dbax83',
-    secretKey: 'q35EwvrEsqnYuxsNlmcAjAPCWuCKQxNkEzuFa4bU'
+    accessKey: '4t4DfspYylq2GefcOTS4',
+    secretKey: 'Ureh7p0PwtLnfN1g8W0Zx8C8uWjCmSbwmUtKRnWn'
   })
 
   const client = { instanceConnect: instanceMinio }
