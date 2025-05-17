21d94b49eb97933cbb2197548693423c2fd771c9
ducvubo <vminhduc8@gmail.com>
2025-05-17 20:20:36 +0700

    Update environment configuration to disable MongoDB to Elasticsearch synchronization. Upgrade axios to version 1.9.0 and add form-data package. Implement new API endpoint for image generation, including error handling and integration with Stability API for image creation and MinIO for storage.

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,16 +1,35 @@
-import { Controller, Get, Inject, OnModuleInit, Post } from '@nestjs/common'
+import { Body, Controller, Get, Inject, OnModuleInit, Post } from '@nestjs/common'
 import { AppService } from './app.service'
 import { ResponseMessage } from './decorator/customize'
 import { ClientGrpc } from '@nestjs/microservices'
 import AppInterface from './app.interface'
+import { ApiBody, ApiOperation } from '@nestjs/swagger'
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @ResponseMessage('Hello World')
   async getHello(): Promise<any> {
     return await this.appService.getHelloGRPC()
+  }
+
+  @Post('/generate-image')
+  @ApiOperation({ summary: 'Generate image from prompt' })
+  @ApiBody({
+    schema: {
+      type: 'object',
+      properties: {
+        promt: {
+          type: 'string',
+          example: 'A beautiful sunset over the mountains',
+        },
+      },
+    },
+  })
+  @ResponseMessage('Generate image successfully')
+  async generateImage(@Body('promt') promt: any) {
+    return await this.appService.generateImage(promt)
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,13 +1,17 @@
-import { Injectable, OnModuleInit } from '@nestjs/common'
+import { BadRequestException, Injectable, OnModuleInit } from '@nestjs/common'
 import { Client, ClientGrpc, Transport } from '@nestjs/microservices'
 import AppInterface from './app.interface'
 import { join } from 'path'
 import { ConfigService } from '@nestjs/config'
 import { firstValueFrom, Observable } from 'rxjs'
+import axios from 'axios'
+import { v4 as uuidv4 } from 'uuid'
+import * as FormData from 'form-data';
+import { getMinio } from './config/minio.config'
 
 @Injectable()
 export class AppService implements OnModuleInit {
-  constructor(private readonly configService: ConfigService) {}
+  constructor(private readonly configService: ConfigService) { }
 
   @Client({
     transport: Transport.GRPC,
@@ -19,6 +23,7 @@ export class AppService implements OnModuleInit {
   })
   client: ClientGrpc
   private grpcService: AppInterface
+  private readonly minio = getMinio().instanceConnect;
 
   // constructor(@Inject('APP_SERVICE') private readonly grpcClient: ClientGrpc) {}
 
@@ -35,6 +40,54 @@ export class AppService implements OnModuleInit {
     } catch (error) {
       console.error('gRPC call error:', error)
       throw error
+    }
+  }
+
+  async generateImage(prompt: string): Promise<any> {
+    try {
+      // 1. Gọi API Stability để tạo ảnh
+      const formData = new FormData();
+      formData.append('prompt', prompt);
+      formData.append('output_format', 'png');
+
+      const stabilityApiKey = "sk-1HVb7GAoer7Xchg4DyrOl2dK5LJP60XKEX88cJUqKFLBPRiO"
+      const response = await axios.post(
+        'https://api.stability.ai/v2beta/stable-image/generate/core',
+        formData,
+        {
+          headers: {
+            ...formData.getHeaders(),
+            Authorization: `Bearer ${stabilityApiKey}`,
+            Accept: 'image/*',
+          },
+          responseType: 'arraybuffer',
+        }
+      );
+
+      const buffer = Buffer.from(response.data);
+
+      // 2. Upload ảnh lên MinIO
+      const bucketName = 'ai-images';
+      const fileExtension = 'png';
+      const fileName = `${Date.now()}-${uuidv4()}.${fileExtension}`;
+      const contentType = 'image/png';
+
+      const bucketExists = await this.minio.bucketExists(bucketName);
+      if (!bucketExists) {
+        await this.minio.makeBucket(bucketName, 'us-east-1');
+      }
+
+      await this.minio.putObject(bucketName, fileName, buffer, buffer.length, {
+        'Content-Type': contentType,
+      });
+
+      return {
+        image_custom: `/api/view-image?bucket=${bucketName}&file=${fileName}`,
+        image_cloud: `/api/view-image?bucket=${bucketName}&file=${fileName}`,
+      };
+    } catch (error) {
+      console.error('generateImage error:', error?.response?.data || error.message);
+      throw new BadRequestException('Failed to generate or upload image');
     }
   }
 }
